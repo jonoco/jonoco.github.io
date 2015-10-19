8ccc0b5972ed54133285f69f716a7e7816b077da
Joshua Cox <joshua.n.cox@gmail.com>
2015-10-18 23:49:39 -0700

    Project routing added

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,10 +5,12 @@ var	Router = ReactRouter.Router; // decides what content to show
 var	Route = ReactRouter.Route;   // object with route info for router
 var	Main = require('./components/main');
 var Blog = require('./components/blog');
+var Project = require('./components/project');
 
 module.exports = (
 	<Router history={new HashHistory}>
 		<Route path='/' component={Main}>
+			<Route path='project/:id' component={Project} />
 			<Route path='blog' component={Blog} />
 		</Route>
 	</Router>
