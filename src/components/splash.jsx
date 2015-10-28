040342351f54ee8323ea6b83cfe98918ad26d6c0
Joshua Cox <joshua.n.cox@gmail.com>
2015-10-28 15:56:05 -0700

    Splash component updated
    - New content and styling, new font

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -4,9 +4,11 @@ module.exports = React.createClass({
 	render: function() {
 		return <div className='splash'>
 			<div className='description'>
-				<p>Joshua Cox is a front-end & mobile developer.
-				 He studied at UCLA and is currently working towards a career
-				  as a front-end developer.
+				<h1>- Hello</h1>
+				<p>I'm <span className='emphasis'>Joshua Cox</span> a 
+				<span className='emphasis'> front-end</span> & <span className='emphasis'>mobile developer </span>
+				with years of experience in <span className='emphasis'>application development</span>.
+				I graduated from UCLA and am currently working towards a career as a front-end developer.
 				</p>
 			</div>
 		</div>
